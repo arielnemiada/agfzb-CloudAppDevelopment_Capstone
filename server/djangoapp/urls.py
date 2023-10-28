06e4867982bc58c916b27f220269a218b8736816
arielnemiada <121762369+arielnemiada@users.noreply.github.com>
2023-10-28 14:33:41 +0800

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,14 +8,20 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    # path for about view
+    path(route='', view=views.home, name='Home'),
 
     # path for about view
+    path(route='about-us', view=views.about, name='About Us'),
 
     # path for contact us view
+    path(route='contact-us', view=views.contact, name='Contact Us'),
 
     # path for registration
+    path(route='registration', view=views.registration_request, name='Registration'),
 
     # path for login
+    path(route='login', view=views.login_request, name='Login'),
 
     # path for logout
 
